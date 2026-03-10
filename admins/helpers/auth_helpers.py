0534613f5ed7c2a8ed5262b97160705ea94215dd
Cosmic <qodirjonov0854@gmail.com>
2026-03-11 02:18:43 +0500

    admin auth is done and can be accessed and working perfectly

--- admins/helpers/auth_helpers.py
+++ admins/helpers/auth_helpers.py
@@ -1,5 +1,4 @@
 import json
-from django.http import JsonResponse
 
 
 def _parse_body(request) -> dict:
@@ -16,25 +15,8 @@ def _get_client_ip(request) -> str:
     return request.META.get("REMOTE_ADDR", "")
 
 
-def _get_session_key(request) -> str:
+def _get_token(request) -> str:
     auth = request.META.get("HTTP_AUTHORIZATION", "")
-    if auth.startswith("Session "):
-        return auth[8:].strip()
-    return request.COOKIES.get("session_key", "")
-
-
-def _session_cookie(response, session_key) -> JsonResponse:
-    response.set_cookie(
-        "session_key",
-        session_key,
-        max_age=72 * 3600,
-        httponly=True,
-        secure=True,
-        samesite="Lax",
-    )
-    return response
-
-
-def _clear_cookie(response) -> JsonResponse:
-    response.delete_cookie("session_key")
-    return response
+    if auth.startswith("Bearer "):
+        return auth[7:].strip()
+    return ""
